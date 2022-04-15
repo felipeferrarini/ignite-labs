9e7014982e6b04b47de4fc44c0e7d17bde307bb1
Felipe Ferrarini <ferrarinifelipe@gmail.com>
2022-04-15 13:33:36 -0300

    reply change to classroom project

---
+++ lab-01/classroom/src/http/auth/authorization.guard.ts
@@ -0,0 +1,48 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { expressJwtSecret } from 'jwks-rsa';
+import JWT from 'express-jwt';
+import { promisify } from 'node:util';
+import { ConfigService } from '@nestjs/config';
+import { GqlExecutionContext } from '@nestjs/graphql';
+
+@Injectable()
+export class AuthorizationGuard implements CanActivate {
+  private AUH0_AUDIENCE: string;
+  private AUTH0_DOMAIN: string;
+
+  constructor(private readonly configService: ConfigService) {
+    this.AUH0_AUDIENCE = this.configService.get('AUTH0_AUDIENCE') ?? '';
+    this.AUTH0_DOMAIN = this.configService.get('AUTH0_DOMAIN') ?? '';
+  }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const { req, res } = GqlExecutionContext.create(context).getContext();
+
+    const checkJWT = promisify(
+      JWT({
+        secret: expressJwtSecret({
+          cache: true,
+          rateLimit: true,
+          jwksRequestsPerMinute: 5,
+          jwksUri: `${this.AUTH0_DOMAIN}.well-known/jwks.json`,
+        }),
+        audience: this.AUH0_AUDIENCE,
+        issuer: this.AUTH0_DOMAIN,
+        algorithms: ['RS256'],
+      }),
+    );
+
+    try {
+      await checkJWT(req, res);
+    } catch (error) {
+      throw new UnauthorizedException(error);
+    }
+
+    return true;
+  }
+}
